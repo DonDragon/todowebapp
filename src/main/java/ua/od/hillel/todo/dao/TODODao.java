72f250788be99a04c0bb1c028d189a9e9f9f3edc
Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
2013-08-28 18:51:10 +0300

    Added hibernate and jsp

---
+++ src/main/java/ua/od/hillel/todo/dao/TODODao.java
@@ -0,0 +1,32 @@
+package ua.od.hillel.todo.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ua.od.hillel.todo.entities.TODOList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class TODODao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<TODOList> findTODOLists() {
+        return entityManager.createQuery(
+                "SELECT l FROM TODOList l").getResultList();
+    }
+
+    public TODOList load(Long id) {
+       return entityManager.find(TODOList.class, id);
+    }
+
+    public void create(Object o) {
+        entityManager.persist(o);
+    }
+}
