6f6b0459141f2e23538328c2d9c038583cc06f9f
Konstantin Nizhegorodov <k.nizhegorodov@gmail.com>
2013-09-12 12:25:27 +0300

    My first unit test =)

---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.od.hillel.todo.dao.TODODao;
+import ua.od.hillel.todo.entities.User;
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        {"classpath:applicationContext.xml",
+         "file:src/main/webapp//WEB-INF/mvc-dispatcher-servlet.xml",
+         "file:src/main/webapp//WEB-INF/spring-security.xml"
+        })
+public class CreateUserTest {
+
+    @Autowired
+    TODODao dao;
+
+    @Test
+    public void test() {
+        User user = new User();
+        user.setUsername("Test User");
+        user.setAge(27);
+        user.setEmail("test@mail.com");
+        user.setIconName("test");
+        user.setPassword("123456");
+
+        dao.create(user);
+
+        assertNotNull(dao.findUserByName("Test User"));
+
+
+    }
+
+}
